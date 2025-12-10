ed0a1437a3351d9175cc26bd35fbabe574f4d22b
haidar <haidarah134@gmail.com>
2025-12-10 12:16:00 +0700

    create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+php artisan serve &
+PID1=$!
+
+php artisan reverb:start &
+PID2=$!
+
+php artisan queue:work &
+PID3=$!
+
+trap "echo 'Stopping...'; kill $PID1 $PID2 $PID3; exit 0" SIGINT
+
+wait
