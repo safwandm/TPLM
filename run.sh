f56d2a58d574c029adaa1ca871bd56f869057696
haidar <haidarah134@gmail.com>
2025-12-17 22:01:38 +0700

    run.sh, delete npm run dev

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ PID2=$!
 php artisan queue:work &
 PID3=$!
 
-npm run dev &
-PID4=$!
+# npm run dev &
+# PID4=$!
 
-trap "echo 'Stopping...'; kill $PID1 $PID2 $PID3 $PID4; exit 0" SIGINT
+trap "echo 'Stopping...'; kill $PID1 $PID2 $PID3; exit 0" SIGINT
 
 wait
